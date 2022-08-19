94243663b8cce52771141310d97ac6faa8bf5714
Antônio Lacerda <108072239+AntLacerda@users.noreply.github.com>
2022-08-19 17:18:01 -0300

    Add files via upload

---
+++ quadro_criativo.js
@@ -0,0 +1,17 @@
+function myFunction(smallimg)
+{
+    var fullimg = document.getElementById("imagebox");
+    fullimg.src = smallimg.src;
+}
+
+function myFunction1(smallimg)
+{
+    var fullimg = document.getElementById("imagebox1");
+    fullimg.src = smallimg.src;
+}
+
+function myFunction2(smallimg)
+{
+    var fullimg = document.getElementById("imagebox2");
+    fullimg.src = smallimg.src;
+}
